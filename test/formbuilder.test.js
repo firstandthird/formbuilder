7929b162b9fe5c7f1c82239f3eb0cb9625dd30d7
Troy Whiteley <troy@somany.us>
2017-01-17 17:32:41 -0800

    Add textarea

--- test/formbuilder.test.js
+++ test/formbuilder.test.js
@@ -29,6 +29,12 @@ test('Input generation', assert => {
         placeholder: 'email@example.com'
       },
       {
+        type: 'textarea',
+        id: 'bio',
+        label: 'Bio',
+        className: 'input'
+      },
+      {
         type: 'submit',
         className: 'button',
         value: 'Submit'
@@ -37,7 +43,7 @@ test('Input generation', assert => {
   });
 
   const html = form.toHTML();
-  const expected = '<form method="POST" action="/api/test/form" class="form"><label for="firstName">First Name</label><input type="text" id="firstName" name="firstName" class="input" value="false" required><label for="email">Email</label><input type="email" id="email" name="email" class="input" value="false" placeholder="email@example.com"><input type="submit" class="button" value="Submit"></form>';
+  const expected = '<form method="POST" action="/api/test/form" class="form"><label for="firstName">First Name</label><input type="text" id="firstName" name="firstName" class="input" value="false" required><label for="email">Email</label><input type="email" id="email" name="email" class="input" value="false" placeholder="email@example.com"><label for="bio">Bio</label><textarea id="bio" name="bio" class="input"></textarea><input type="submit" class="button" value="Submit"></form>';
   assert.equal(html, expected, 'HTML as expected');
 
   assert.end();
